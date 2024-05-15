a080754c252c27ad647819863fcc0b9e8b69fd17
Zarif Ceaser <zarifceaser321@gmail.com>
2024-05-15 15:08:58 -0500

    ran output to training script

---
+++ out_to_training_script.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+import sound
+
+out_path = Path.cwd()/'out'
+urination_out_path = out_path/'urination'
+
+for file in os.listdir(urination_out_path):
+    input_file = os.path.join(f'out/urination/',file)
+    output_file = 'training_set/urination'
+    sound.quantize_read_write(input_file=input_file, output=output_file, output_file_extension='wav', bits=1)
