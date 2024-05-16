426a2f1974b975ebce18c54a5fd2818d89ad8eff
Zarif Ceaser <zarifceaser321@gmail.com>
2024-05-15 22:37:37 -0500

    changed urination data to 3 bit

--- out_to_training_script.py
+++ out_to_training_script.py
@@ -8,4 +8,4 @@ urination_out_path = out_path/'urination'
 for file in os.listdir(urination_out_path):
     input_file = os.path.join(f'out/urination/',file)
     output_file = 'training_set/urination'
-    sound.quantize_read_write(input_file=input_file, output=output_file, output_file_extension='wav', bits=1)
+    sound.quantize_read_write(input_file=input_file, output=output_file, output_file_extension='wav', bits=3)
